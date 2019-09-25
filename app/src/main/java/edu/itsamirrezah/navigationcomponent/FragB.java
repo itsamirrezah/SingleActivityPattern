d8b02e5a4a055d496c77f0aa3b8cb0170aa71871
itsamirrezah <soheil.irani7@gmail.com>
2019-09-25 17:44:25 +0330

    Pass data between destinations

--- app/src/main/java/edu/itsamirrezah/navigationcomponent/FragB.java
+++ app/src/main/java/edu/itsamirrezah/navigationcomponent/FragB.java
@@ -2,12 +2,14 @@ package edu.itsamirrezah.navigationcomponent;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 
 /**
@@ -15,6 +17,8 @@ import android.view.ViewGroup;
  */
 public class FragB extends Fragment {
 
+
+    TextView tvUsername, tvPassword;
 
     public FragB() {
         // Required empty public constructor
@@ -25,7 +29,21 @@ public class FragB extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_b, container, false);
+        View view = inflater.inflate(R.layout.fragment_frag_b, container, false);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvPassword = view.findViewById(R.id.tvPassword);
+
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String username = FragBArgs.fromBundle(getArguments()).getUsername();
+        String password = FragBArgs.fromBundle(getArguments()).getPassword();
+
+        tvUsername.setText(username.length() > 0 ? username : "NO-USERNAME");
+        tvPassword.setText(password.length() > 0 ? tvPassword.getText().toString() + password : tvPassword.getText().toString() + "NO-PASSWORD");
+    }
 }
