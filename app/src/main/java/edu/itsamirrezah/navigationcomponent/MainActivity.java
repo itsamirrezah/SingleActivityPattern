718241e040dd0a3207e78a9a65d46708d301b119
itsamirrezah <soheil.irani7@gmail.com>
2019-09-25 18:48:38 +0330

    BottomNavigationView functionality

--- app/src/main/java/edu/itsamirrezah/navigationcomponent/MainActivity.java
+++ app/src/main/java/edu/itsamirrezah/navigationcomponent/MainActivity.java
@@ -1,14 +1,25 @@
 package edu.itsamirrezah.navigationcomponent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+
+        NavController navController = Navigation.findNavController(this, R.id.mainNavHost);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
